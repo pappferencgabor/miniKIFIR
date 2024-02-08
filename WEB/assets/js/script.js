fa18357e70006b11ac36f1349ae5628946ee0a1a
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2024-02-08 11:04:09 +0100

    fejlesztesek

--- WEB/assets/js/script.js
+++ WEB/assets/js/script.js
@@ -5,14 +5,13 @@ let pointtxt = document.getElementById("inputPoints");
 function loadResults(data) {
     let resTable = document.getElementById("results");
     resTable.innerHTML = `
-        <tr> <th>OM</th>
-            <th>Név</th>
-            <th>Matek</th>
-            <th>Magyar</th>
-            <th>Összesen</th>
+        <tr> <th onclick="sortByKeyClick(allData, 'OM_Azonosito')">OM</th>
+            <th onclick="sortByKeyClick(allData, 'Neve')">Név</th>
+            <th onclick="sortByKeyClick(allData, 'Matematika')">Matek</th>
+            <th onclick="sortByKeyClick(allData, 'Magyar')">Magyar</th>
+            <th onclick="sortBySum(allData)">Összesen</th>
         </tr>`;
 
-    sortByKey(data, 'Neve')
     data.forEach(tanulo => {
         resTable.innerHTML +=
             `
@@ -45,13 +44,42 @@ function showInputs() {
 }
 
 function checkFilters() {
+    let omcb = document.getElementById("inputOMCB").checked;
+    let nevcb = document.getElementById("inputNameCB").checked;
+    let filteredList = [];
 
+    if (omcb || omtxt.value.trim() && nevcb) {
+        allData.forEach(tanulo => {
+            if (tanulo.Neve.startsWith(nametxt) && tanulo.OM_Azonosito.startsWith(omtxt)) {
+                filteredList.push(tanulo);
+            }
+        });
+    }
+    else if (omcb && omtxt.value.trim() != "") {
+        allData.forEach(tanulo => {
+            if (tanulo.OM_Azonosito.startsWith(omtxt.value)) {
+                filteredList.push(tanulo);
+            }
+        })
+    }
+    else if (nevcb && nametxt.value.trim() != "") {
+        allData.forEach(tanulo => {
+            if (tanulo.Neve.startsWith(nametxt.value)) {
+                filteredList.push(tanulo);
+            }
+        })
+    }
+    else if (omtxt.value.trim() == "" && nametxt.value.trim() == "") {
+        filteredList = allData;
+    }
+    loadResults(filteredList);
 }
 
 function checkPoints() {
     if (pointtxt.value.trim() == "" || parseInt(pointtxt.value) < 0) {
         pointtxt.value = 0
         loadResults(allData);
+        displayStats(allData);
     } else {
         let filteredList = [];
 
@@ -61,7 +89,35 @@ function checkPoints() {
             }
         });
         loadResults(filteredList);
+        displayStats(filteredList);
     }
+}
+
+function displayStats(array) {
+    let tanulokSzama = document.getElementById("tanulokSzama");
+    let matekAtlag = document.getElementById("matekAtlag");
+    let magyarAtlag = document.getElementById("magyarAtlag");
+    let osszAtlag = document.getElementById("osszAtlag");
+
+    let matSzum = 0;
+    let magySzum = 0;
+    let osszSzum = 0;
+
+    array.forEach(tanulo => {
+        matSzum += tanulo.Matematika;
+        magySzum += tanulo.Magyar;
+        osszSzum += (tanulo.Matematika + tanulo.Magyar);
+        console.log(osszSzum)
+    });
+
+    console.log(matSzum)
+    console.log(magySzum)
+    console.log(osszSzum)
+
+    tanulokSzama.innerHTML = array.length;
+    matekAtlag.innerHTML = (matSzum / array.length).toFixed(1);
+    magyarAtlag.innerHTML = (magySzum / array.length).toFixed(1);
+    osszAtlag.innerHTML = (osszSzum / array.length).toFixed(1);
 
 }
 
@@ -72,4 +128,20 @@ function sortByKey(array, key) {
     });
 }
 
+function sortByKeyClick(array, key) {
+    loadResults(array.sort(function (a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    }));
+}
+
+function sortBySum(array) {
+    loadResults(array.sort(function (a, b) {
+        var x = a['Matematika'] + a['Magyar']; var y = b['Matematika'] + b['Magyar'];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    }));
+}
+
+sortByKey(allData, 'Neve');
 loadResults(allData);
+displayStats(allData);
