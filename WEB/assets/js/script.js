222392e2190eadc8290034b92f36a9bf8f557a95
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2024-02-09 15:31:53 +0100

    meg tobb fejlesztes (B es D resz kesz)

--- WEB/assets/js/script.js
+++ WEB/assets/js/script.js
@@ -1,28 +1,28 @@
 let omtxt = document.getElementById("inputOM");
 let nametxt = document.getElementById("inputName");
 let pointtxt = document.getElementById("inputPoints");
+let resTable = document.getElementById("results");
+let resCSV = document.getElementById("csvOut");
 
 function loadResults(data) {
-    let resTable = document.getElementById("results");
     resTable.innerHTML = `
-        <tr> <th onclick="sortByKeyClick(allData, 'OM_Azonosito')">OM</th>
-            <th onclick="sortByKeyClick(allData, 'Neve')">Név</th>
-            <th onclick="sortByKeyClick(allData, 'Matematika')">Matek</th>
-            <th onclick="sortByKeyClick(allData, 'Magyar')">Magyar</th>
-            <th onclick="sortBySum(allData)">Összesen</th>
+        <tr> 
+            <th onclick="sortByKeyClick(allData, 'OM_Azonosito')" id="thOM_Azonosito">OM</th>
+            <th onclick="sortByKeyClick(allData, 'Neve')" id="thNeve">Név</th>
+            <th onclick="sortByKeyClick(allData, 'Matematika')" id="thMatematika">Matematika</th>
+            <th onclick="sortByKeyClick(allData, 'Magyar')" id="thMagyar">Magyar</th>
+            <th onclick="sortBySum(allData, 'Osszesen')" id="thOsszesen">Összesen</th>
         </tr>`;
 
     data.forEach(tanulo => {
-        resTable.innerHTML +=
-            `
-            <tr>
+        resTable.innerHTML += `
+            <tr ondblclick="CSVLine(${tanulo.OM_Azonosito})">
                 <td>${tanulo.OM_Azonosito}</td>
                 <td>${tanulo.Neve}</td>
                 <td>${tanulo.Matematika}</td>
                 <td>${tanulo.Magyar}</td>
                 <td>${tanulo.Matematika + tanulo.Magyar}</td>
-            </tr>
-        `;
+            </tr>`;
     });
 }
 
@@ -107,18 +107,24 @@ function displayStats(array) {
         matSzum += tanulo.Matematika;
         magySzum += tanulo.Magyar;
         osszSzum += (tanulo.Matematika + tanulo.Magyar);
-        console.log(osszSzum)
     });
 
-    console.log(matSzum)
-    console.log(magySzum)
-    console.log(osszSzum)
+    console.log(array.length > 0);
 
     tanulokSzama.innerHTML = array.length;
-    matekAtlag.innerHTML = (matSzum / array.length).toFixed(1);
-    magyarAtlag.innerHTML = (magySzum / array.length).toFixed(1);
-    osszAtlag.innerHTML = (osszSzum / array.length).toFixed(1);
+    matekAtlag.innerHTML = array.length > 0 ? (matSzum / array.length).toFixed(1) : "0";
+    magyarAtlag.innerHTML = array.length > 0 ? (magySzum / array.length).toFixed(1) : "0";
+    osszAtlag.innerHTML = array.length > 0 ? (osszSzum / array.length).toFixed(1) : "0";
+    resTable.style.display = array.length > 0 ? "table" : "none";
+}
 
+function CSVLine(om) {
+    allData.forEach(tanulo => {
+        if (tanulo.OM_Azonosito == om) {
+            resCSV.innerHTML += `${tanulo.OM_Azonosito};${tanulo.Neve};${tanulo.ErtesitesiCime};${tanulo.Email};${tanulo.Matematika};${tanulo.Magyar}<br>`;
+            resCSV.scrollIntoView();
+        }
+    });
 }
 
 function sortByKey(array, key) {
@@ -129,17 +135,20 @@ function sortByKey(array, key) {
 }
 
 function sortByKeyClick(array, key) {
+    console.log(document.getElementById(`th${key}`));
+    document.getElementById(`th${key}`).innerHTML += " ▲"
     loadResults(array.sort(function (a, b) {
         var x = a[key]; var y = b[key];
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     }));
 }
 
-function sortBySum(array) {
+function sortBySum(array, key) {
     loadResults(array.sort(function (a, b) {
         var x = a['Matematika'] + a['Magyar']; var y = b['Matematika'] + b['Magyar'];
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     }));
+    document.getElementById(`th${key}`).innerHTML += " ▲"
 }
 
 sortByKey(allData, 'Neve');
